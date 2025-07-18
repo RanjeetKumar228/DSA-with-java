0bd7b5877d1f3780201fe6fa599220b580b2bd11
Ranjeet <example@gmail.com>
2025-07-18 15:56:55 +0530

    Sum by function

--- Java-Programs/Method/Code/src/com/ranjeet/Sum.java
+++ Java-Programs/Method/Code/src/com/ranjeet/Sum.java
@@ -1,8 +1,23 @@
 package com.ranjeet;
 
+import java.util.Scanner;
+
 public class Sum {
 
     public static void main(String[] args) {
+        sum();
+    }
+
+    static void sum (){
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the first number : ");
+        int num1 = in.nextInt();
+        System.out.print("Enter the second number : ");
+        int num2 = in.nextInt();
+
+        int sum = num1 + num2;
+        System.out.println(sum);
 
     }
 }
