42dff42915b116c04e73e7f9d45c8085081f31f3
Ranjeet kumar <ranjeet78827@gmail.com>
2025-07-18 23:44:40 +0530

    Swap two num but not possible

---
+++ Java-Programs/Method/Code/src/com/ranjeet/SWT.java
@@ -0,0 +1,30 @@
+package com.ranjeet;
+
+import java.util.Scanner;
+
+public class SWT {
+    public static void main(String[] args) {
+
+//        int a = 10;
+//        int b = 20;
+//        swap(a,b);   // it can't swap
+//        System.out.println(a + " " + b);
+
+        String name = "Ranjeet";
+        changeName(name);
+        System.out.println(name);
+    }
+
+    static  void changeName (String namm){
+        String name = "Jeet";
+    }
+
+
+    static void swap(int a, int b){
+        // Swap the number
+
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+}
