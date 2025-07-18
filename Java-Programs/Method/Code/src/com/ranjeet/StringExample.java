72fbf97b5a162331f19b4873e0e0a9a3de62fb42
Ranjeet <example@gmail.com>
2025-07-18 17:03:01 +0530

    String example for greeting program

---
+++ Java-Programs/Method/Code/src/com/ranjeet/StringExample.java
@@ -0,0 +1,12 @@
+package com.ranjeet;
+
+public class StringExample {
+    public static void main(String[] args) {
+        String message = greet();
+        System.out.println(message);
+    }
+    static  String greet () {
+        String greeting = "Hello Ranjeet";
+        return  greeting;
+    }
+}
