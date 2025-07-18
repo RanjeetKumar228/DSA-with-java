a60378059e4c9dfcac737efc17e8eb4162efddd7
Ranjeet <example@gmail.com>
2025-07-18 15:34:54 +0530

    Syntax of method

---
+++ Java-Programs/Method/Code/src/com/ranjeet/Syntax.java
@@ -0,0 +1,15 @@
+package com.ranjeet;
+
+public class Syntax {
+
+    public static void main(String[] args) {
+
+        /*
+         Syntax of the method
+
+         return_type name () {
+              //Statement
+         }
+         */
+    }
+}
