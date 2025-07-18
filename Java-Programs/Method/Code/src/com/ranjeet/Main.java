90a97a7972215c9d76d14534cb7f6c9d0ad3ae7c
Ranjeet <example@gmail.com>
2025-07-18 15:27:45 +0530

    Start function

---
+++ Java-Programs/Method/Code/src/com/ranjeet/Main.java
@@ -0,0 +1,114 @@
+package com.ranjeet;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Take input of two number and print the sum.
+
+        Scanner in = new Scanner(System.in);
+
+        int num1, num2, sum;
+
+        System.out.print("Enter the first number : ");
+        num1 = in.nextInt();
+        System.out.print("Enter the second number : ");
+        num2 = in.nextInt();
+
+        sum = num1 + num2;
+        System.out.print("The result is : " + sum);
+        System.out.println(/*space*/);
+        System.out.println(/*space*/);
+
+
+        System.out.print("Enter the first number : ");
+        num1 = in.nextInt();
+        System.out.print("Enter the second number : ");
+        num2 = in.nextInt();
+
+        sum = num1 + num2;
+        System.out.print("The result is : " + sum);
+        System.out.println(/*space*/);
+        System.out.println(/*space*/);
+
+
+        System.out.print("Enter the first number : ");
+        num1 = in.nextInt();
+        System.out.print("Enter the second number : ");
+        num2 = in.nextInt();
+
+        sum = num1 + num2;
+        System.out.print("The result is : " + sum);
+        System.out.println(/*space*/);
+        System.out.println(/*space*/);
+
+
+        System.out.print("Enter the first number : ");
+        num1 = in.nextInt();
+        System.out.print("Enter the second number : ");
+        num2 = in.nextInt();
+
+        sum = num1 + num2;
+        System.out.print("The result is : " + sum);
+        System.out.println(/*space*/);
+        System.out.println(/*space*/);
+
+
+        System.out.print("Enter the first number : ");
+        num1 = in.nextInt();
+        System.out.print("Enter the second number : ");
+        num2 = in.nextInt();
+
+        sum = num1 + num2;
+        System.out.print("The result is : " + sum);
+        System.out.println(/*space*/);
+        System.out.println(/*space*/);
+
+
+        System.out.print("Enter the first number : ");
+        num1 = in.nextInt();
+        System.out.print("Enter the second number : ");
+        num2 = in.nextInt();
+
+        sum = num1 + num2;
+        System.out.print("The result is : " + sum);
+        System.out.println(/*space*/);
+        System.out.println(/*space*/);
+
+
+        System.out.print("Enter the first number : ");
+        num1 = in.nextInt();
+        System.out.print("Enter the second number : ");
+        num2 = in.nextInt();
+
+        sum = num1 + num2;
+        System.out.print("The result is : " + sum);
+        System.out.println(/*space*/);
+        System.out.println(/*space*/);
+
+
+        System.out.print("Enter the first number : ");
+        num1 = in.nextInt();
+        System.out.print("Enter the second number : ");
+        num2 = in.nextInt();
+
+        sum = num1 + num2;
+        System.out.print("The result is : " + sum);
+        System.out.println(/*space*/);
+        System.out.println(/*space*/);
+
+
+        System.out.print("Enter the first number : ");
+        num1 = in.nextInt();
+        System.out.print("Enter the second number : ");
+        num2 = in.nextInt();
+
+        sum = num1 + num2;
+        System.out.print("The result is : " + sum);
+        System.out.println(/*space*/);
+        System.out.println(/*space*/);
+
+    }
+}
---
+++ Java-Programs/Method/Code/src/com/ranjeet/Sum.java
@@ -0,0 +1,8 @@
+package com.ranjeet;
+
+public class Sum {
+
+    public static void main(String[] args) {
+
+    }
+}
