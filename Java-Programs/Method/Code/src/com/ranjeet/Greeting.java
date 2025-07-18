1e1e59b14e490d2d7848678a43da6b8c54fb0b01
Ranjeet <example@gmail.com>
2025-07-18 17:00:24 +0530

    Greeting program

---
+++ Java-Programs/Method/Code/src/com/ranjeet/Greeting.java
@@ -0,0 +1,16 @@
+package com.ranjeet;
+
+import java.util.Scanner;
+
+public class Greeting {
+
+    public static void main(String[] args) {
+        greeting();
+    }
+
+    static void greeting () {
+        System.out.println("Hello world");
+    }
+
+
+}
