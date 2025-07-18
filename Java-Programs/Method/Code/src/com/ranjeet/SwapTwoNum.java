3deee578472fe0ec0c5a4bc579f9e9bea0d7a4d5
Ranjeet kumar <ranjeet78827@gmail.com>
2025-07-18 23:26:59 +0530

    Swap two number program

--- Java-Programs/Method/Code/src/com/ranjeet/SwapTwoNum.java
+++ Java-Programs/Method/Code/src/com/ranjeet/SwapTwoNum.java
@@ -1,4 +1,19 @@
 package com.ranjeet;
 
+import java.util.Scanner;
+
 public class SwapTwoNum {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        // Swap the number
+
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println(a + " " + b);
+    }
 }
