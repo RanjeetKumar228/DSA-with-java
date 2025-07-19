7ba4aa6971448e2727bf24cbbda3a3147ea345ab
Ranjeet kumar <ranjeet78827@gmail.com>
2025-07-19 17:45:51 +0530

    What is shadowing ? done✅

---
+++ Java-Programs/Method/Code/src/com/ranjeet/Shadowing.java
@@ -0,0 +1,21 @@
+package com.ranjeet;
+
+public class Shadowing {
+   static int x = 90;  // this will be shadowed at line 8
+    public static void main(String[] args) {
+        System.out.println(x);  // 90
+        int x;   // the class variable at line 4 is shadowed by this.
+        // System.out.println(x);     // scope will begin when variable is initialised.
+        x = 40;
+        System.out.println(x);  // 40
+        fun();
+    }
+
+    static void fun (){
+        System.out.println(x);
+    }
+}
+
+ /*                              What is shadowing ?
+     Shadowing is basically , a practice of using two variable with a same name within the scope that overlap.
+  */
