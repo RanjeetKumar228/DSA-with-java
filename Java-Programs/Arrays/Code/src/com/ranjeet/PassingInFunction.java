10f063d8dfb115b0d4fb3eccd2acca3bea612664
Ranjeet kumar <ranjeet78827@gmail.com>
2025-07-21 21:16:53 +0530

    Passing in function done

---
+++ Java-Programs/Arrays/Code/src/com/ranjeet/PassingInFunction.java
@@ -0,0 +1,19 @@
+package com.ranjeet;
+
+import java.util.Arrays;
+
+public class PassingInFunction {
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4,5,6};
+        System.out.println(Arrays.toString(arr));
+        change(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+
+     static void change(int[] arr) {
+        arr[0] = 99;
+    }
+
+}
