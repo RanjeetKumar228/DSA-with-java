10fe8a3a7b022ae3009f4ed9af0ee267ac536505
Ranjeet kumar <ranjeet78827@gmail.com>
2025-07-22 15:36:19 +0530

    Multidimentional Array List done

---
+++ Java-Programs/Arrays/Code/src/com/ranjeet/MultiDimensionAL.java
@@ -0,0 +1,29 @@
+package com.ranjeet;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MultiDimensionAL {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        //Syntax
+        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+
+        // initialisation
+        for (int i = 0; i < 3; i++) {
+            list.add(new ArrayList<>());
+
+        }
+
+        // add element
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                list.get(i).add(in.nextInt());
+            }
+        }
+        System.out.println(list);
+    }
+}
