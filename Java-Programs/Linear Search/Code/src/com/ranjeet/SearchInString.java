3db881f3364f8ad77b41e08529c632bffb5a0047
Ranjeet kumar <ranjeet78827@gmail.com>
2025-07-25 23:19:27 +0530

    Q : Search in String

---
+++ Java-Programs/Linear Search/Code/src/com/ranjeet/SearchInString.java
@@ -0,0 +1,38 @@
+package com.ranjeet;
+
+import java.util.Arrays;
+
+public class SearchInString {
+    public static void main(String[] args) {
+        String name = "Ranjeet";
+        char target = 't';
+        System.out.println(searchString(name,target));
+        System.out.println(Arrays.toString(name.toCharArray()));
+    }
+
+    static boolean searchString2 (String str, char target) {
+        if (str.isEmpty()) {
+            return false;
+        }
+        for (char ch : str.toCharArray()){
+            if(ch == target){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //Search target and return true or false
+
+    static boolean searchString (String str, char target){
+        if(str.isEmpty()){
+            return false;
+        }
+        for (int i = 0; i < str.length(); i++) {
+            if (target == str.charAt(i)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
