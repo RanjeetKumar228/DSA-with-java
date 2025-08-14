8ab779ed4931d8d60990656b0839f068ab083b97
Ranjeet kumar <ranjeet78827@gmail.com>
2025-08-14 16:16:14 +0530

    Search in Matrixes

--- Java-Programs/Binary Search in 2D Array/code/src/com/ranjeet/Main.java
+++ Java-Programs/Binary Search in 2D Array/code/src/com/ranjeet/Main.java
@@ -1,17 +1,29 @@
 package com.ranjeet;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int[][] arr = {
+                {10, 20, 30},
+                {40, 50, 60},
+                {70, 80, 90},
+        };
+        System.out.println(Arrays.toString(BS2DArray(arr,900)));
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    static int[] BS2DArray(int[][] arr, int target) {
+        int row = 0;
+        int col = arr[0].length - 1;
+        while (row < arr.length && col >= 0){
+            if (arr[row][col] == target){
+                return new int[] {row,col};
+            } else if (arr[row][col] > target){
+                col--;
+            } else {
+                row++;
+            }
         }
+        return new int[] {-1,-1};
     }
 }
