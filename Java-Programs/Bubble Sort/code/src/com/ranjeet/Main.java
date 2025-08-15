6325f4cbee0428f864c6aa8d366062f5592907b2
Ranjeet kumar <ranjeet78827@gmail.com>
2025-08-15 20:45:54 +0530

    Bubble Sort

--- Java-Programs/Bubble Sort/code/src/com/ranjeet/Main.java
+++ Java-Programs/Bubble Sort/code/src/com/ranjeet/Main.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5};
+        int[] arr = {1,2,3,4,5,6};
        BubbleSort(arr);
         System.out.println(Arrays.toString(arr));
     }
