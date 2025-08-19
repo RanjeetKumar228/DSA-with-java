b771e6f025bbbb4746d66abf7122e0c122b78509
Ranjeet kumar <ranjeet78827@gmail.com>
2025-08-19 07:31:25 +0530

    Q: find missing no. (LeetCode 268)

---
+++ Java-Programs/Sorting/code/src/com/ranjeet/missingNo.java
@@ -0,0 +1,35 @@
+package com.ranjeet;
+
+class missingNo {
+    public static void main (String[] args) {
+        int[] arr = {7,5,4,0,1,2};
+        int ans = missingNumber(arr);
+        System.out.println(ans);
+    }
+
+    static int missingNumber(int[] arr){
+        int i = 0;
+        while (i < arr.length){
+            int correctIndex = arr[i];
+            if (arr[i] < arr.length && arr[i] != arr[correctIndex]) {
+                swap(arr, i, correctIndex);
+            } else {
+                i++;
+            }
+        }
+
+        // find the missing number
+        for (int j = 0; j < arr.length; j++) {
+            if (arr[j] != j) {
+                return j;
+            }
+        }
+        return arr.length;
+    }
+
+    static void swap(int[] arr, int i, int correctPosition) {
+        int temp = arr[correctPosition];
+        arr[correctPosition] = arr[i];
+        arr[i] = temp;
+    }
+}
