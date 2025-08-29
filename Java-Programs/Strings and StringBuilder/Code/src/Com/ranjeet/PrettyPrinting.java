6ba34372c65b2fd62a32466081f2062d146489e8
Ranjeet kumar <ranjeet228k@gmail.com>
2025-08-29 19:07:44 +0530

    Preety Printing like float , String etc..

---
+++ Java-Programs/Strings and StringBuilder/Code/src/Com/ranjeet/PrettyPrinting.java
@@ -0,0 +1,26 @@
+package Com.ranjeet;
+
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 3453.3454f;
+//        System.out.printf("Floated number is %.2f",a);
+
+        System.out.printf("Pi %.3f : ", Math.PI);
+    }
+}
+
+/*
+
+       %c - Character
+       %d - Decimal number (base 10)
+       %e - Exponential floating-point number
+       %f - Floating-point number
+       %i - Integer (base 10)
+       %o - Octal number (base 8)
+       %s - String
+       %u - Unsigned decimal (integer) number
+       %x - Hexadecimal number (base 16)
+       %t - Date/time
+       %n - Newline
+
+ */
