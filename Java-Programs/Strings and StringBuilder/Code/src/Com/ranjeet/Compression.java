7267aae36b9e41b8b8293849b7c5ead52e2e6e90
Ranjeet kumar <ranjeet228k@gmail.com>
2025-08-29 19:04:09 +0530

    == vs .equal()

---
+++ Java-Programs/Strings and StringBuilder/Code/src/Com/ranjeet/Compression.java
@@ -0,0 +1,18 @@
+package Com.ranjeet;
+
+public class Compression {
+    public static void main(String[] args) {
+//        String a = "Ranjeet";
+//        String b = "Ranjeet";
+//        System.out.println(a==b);
+
+        String name1 = new String("Ranjeet");
+        String name2 = new String("Ranjeet  ");
+
+        System.out.println(name1 == name2);
+
+        System.out.println(name1.equals(name2));
+
+        System.out.println(name1.charAt(0));
+    }
+}
