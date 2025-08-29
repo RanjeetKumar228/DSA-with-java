f35c47a48cb00a9104873f2ba24806765e4c6712
Ranjeet kumar <ranjeet228k@gmail.com>
2025-08-29 19:05:46 +0530

    Intro

--- Java-Programs/Strings and StringBuilder/Code/src/Com/ranjeet/Main.java
+++ Java-Programs/Strings and StringBuilder/Code/src/Com/ranjeet/Main.java
@@ -6,7 +6,12 @@ public class Main {
         // int[] arr = {2,3,4,5,6,7};
 
         String name = "Ranjeet Kumar";
-        System.out.println(name);
+        // System.out.println(name);
 
+        String a = "Ranjeet";
+        System.out.println(a);
+
+        a = "Kumar";
+        System.out.println(a);
     }
 }
