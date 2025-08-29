e8ec626180806d51233d8b77efd1fc22688359b5
Ranjeet kumar <ranjeet228k@gmail.com>
2025-08-29 19:06:37 +0530

    Output

---
+++ Java-Programs/Strings and StringBuilder/Code/src/Com/ranjeet/Output.java
@@ -0,0 +1,16 @@
+package Com.ranjeet;
+import java.util.Arrays;
+
+public class Output {
+    public static void main(String[] args) {
+
+//        System.out.println(56);
+//        Integer num = Integer.valueOf(6);
+//        System.out.println(num.toString());
+        System.out.println("Ranjeet");
+        System.out.println(Arrays.toString(new int[]{2,3,4,5,67}));
+
+//        String name = null;
+//        System.out.println(name);
+    }
+}
