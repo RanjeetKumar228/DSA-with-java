9a4b06025d8d0e9840d7ef845a10aa6d82d85a9b
Ranjeet kumar <ranjeet228k@gmail.com>
2025-09-07 18:09:21 +0530

    pattern question

--- Java-Programs/Pattern/Code/src/com/ranjeet/Main.java
+++ Java-Programs/Pattern/Code/src/com/ranjeet/Main.java
@@ -2,10 +2,19 @@ package com.ranjeet;
 
 public class Main {
     public static void main(String[] args) {
-        pattern2(5);
+        pattern5(5);
     }
 
     static void pattern1(int n){
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern2(int n){
         for (int row = 1; row <= n; row++){
             for (int col = 1; col <= row; col++){
                 System.out.print("* ");
@@ -14,9 +23,28 @@ public class Main {
         }
     }
 
-    static void pattern2(int n){
+    static void pattern3(int n){
         for (int row = 1; row <= n; row++) {
             for (int col = 1; col <= n+1-row; col++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern4(int n){
+        for (int row = 1; row < n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern5(int n){
+        for (int row = 0; row < 2*n; row++) {
+            int totalCol = row > n ? 2*n - row : row;
+            for (int col = 0; col < totalCol; col++) {
                 System.out.print("* ");
             }
             System.out.println();
