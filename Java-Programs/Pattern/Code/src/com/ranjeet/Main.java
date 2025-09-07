3fc2cfb0358d9e5b7675e4830ec85fef588822d8
Ranjeet kumar <ranjeet228k@gmail.com>
2025-09-07 19:17:57 +0530

    pattern question

--- Java-Programs/Pattern/Code/src/com/ranjeet/Main.java
+++ Java-Programs/Pattern/Code/src/com/ranjeet/Main.java
@@ -2,7 +2,7 @@ package com.ranjeet;
 
 public class Main {
     public static void main(String[] args) {
-        pattern5(5);
+        pattern17(4);
     }
 
     static void pattern1(int n){
@@ -46,6 +46,53 @@ public class Main {
             int totalCol = row > n ? 2*n - row : row;
             for (int col = 0; col < totalCol; col++) {
                 System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern28(int n){
+        for (int row = 0; row < 2*n; row++) {
+            int totalCol = row > n ? 2*n - row : row;
+            int noOfSpaces = n - totalCol;
+            for (int s = 0; s < noOfSpaces; s++) {
+                System.out.print(" ");
+            }
+            for (int col = 0; col < totalCol; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern17(int n){
+        for (int row = 1; row <= 2*n; row++) {
+
+            int c = row > n ? 2*n - row - 1 : row;
+
+            for (int s = 0; s < n-c; s++) {
+                System.out.print("  ");
+            }
+            for (int col = c; col >= 1 ; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= c; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void pattern30(int n){
+        for (int row = 1; row <= n; row++) {
+            for (int s = 0; s < n-row; s++) {
+                System.out.print("  ");
+            }
+            for (int col = row; col >= 1 ; col--) {
+                System.out.print(col + " ");
+            }
+            for (int col = 2; col <= row; col++) {
+                System.out.print(col + " ");
             }
             System.out.println();
         }
