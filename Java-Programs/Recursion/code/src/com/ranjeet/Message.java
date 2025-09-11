a93f31bafb16eccfdba0964eaf57da8a7a8e0ccf
Ranjeet kumar <ranjeet228k@gmail.com>
2025-09-11 23:11:17 +0530

    Message Program

---
+++ Java-Programs/Recursion/code/src/com/ranjeet/Message.java
@@ -0,0 +1,32 @@
+package com.ranjeet;
+
+public class Message {
+    public static void main(String[] args) {
+        message();
+
+    }
+    // write a function to print a message
+    static void message(){
+        System.out.println("Hello Ranjeet");
+        message2();
+    }
+
+    static void message2(){
+        System.out.println("Hello Ranjeet");
+        message3();
+    }
+
+    static void message3(){
+        System.out.println("Hello Ranjeet");
+        message4();
+    }
+
+    static void message4(){
+        System.out.println("Hello Ranjeet");
+        message5();
+    }
+
+    static void message5(){
+        System.out.println("Hello Ranjeet");
+    }
+}
