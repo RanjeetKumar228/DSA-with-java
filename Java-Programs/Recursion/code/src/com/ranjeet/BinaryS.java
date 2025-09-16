9cf052875373315bc6d997b55a4563b0394d123c
Ranjeet kumar <ranjeet228k@gmail.com>
2025-09-16 06:58:17 +0530

    BS with recursion

--- Java-Programs/Recursion/code/src/com/ranjeet/BinaryS.java
+++ Java-Programs/Recursion/code/src/com/ranjeet/BinaryS.java
@@ -2,8 +2,19 @@ package com.ranjeet;
 
 public class BinaryS {
     public static void main(String[] args) {
+        int[] arr = {5};
+        int target = 5;
+        System.out.println(bsRecursion(arr,target,0, arr.length-1));
 
     }
 
-
+    static int bsRecursion(int[] arr, int target, int start, int end){
+        if(start > end) return -1;
+        int mid = start + (end - start) / 2;
+        if (arr[mid] == target) return  mid;
+        if (target < arr[mid]) {
+            return bsRecursion(arr, target, start, mid-1);
+        }
+        return bsRecursion(arr,target,mid+1,end);
+    }
 }
