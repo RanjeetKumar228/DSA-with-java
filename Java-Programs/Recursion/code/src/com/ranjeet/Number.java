651e7fbc9dd08a064e8bfa217c0856eaca5c77b6
Ranjeet <ranjeet228k@gmail.com>
2026-05-19 16:06:16 +0530

    Number program

--- Java-Programs/Recursion/code/src/com/ranjeet/Number.java
+++ Java-Programs/Recursion/code/src/com/ranjeet/Number.java
@@ -2,9 +2,11 @@ package com.ranjeet;
 
 public class Number {
     public static void main(String[] args) {
+        // write a function that takes in a number and prints it
+        // print first 5 number 1 2 3 4 5
         number();
     }
-    // write a function to print a number
+
     static void number(){
         System.out.println(1);
         number2();
